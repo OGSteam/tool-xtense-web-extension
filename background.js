f1b93ed9713f21dd8bbae7c54c61d594fbf089b5
Anthony <darknoon29@users.noreply.github.com>
2016-05-12 23:17:58 +0200

    Affichage icone de status dans la barre d'outil du navigateur

---
+++ background.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Anthony on 12/05/2016.
+ */
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // read `newIconPath` from request and read `tab.id` from sender
+        chrome.browserAction.setIcon({
+            path: request.newIconPath,
+            tabId: sender.tab.id
+        });
+    });
