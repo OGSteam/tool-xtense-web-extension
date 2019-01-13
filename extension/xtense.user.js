fca709bfeb7a04bbf16e2886f75e3dc5775ec96b
darknoon29 <darknoon@darkcity.fr>
2019-01-13 17:13:00 +0100

    Bump 2.7.18

--- extension/xtense.user.js
+++ extension/xtense.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name	    Xtense-GM
-// @version     2.7.13
+// @version     2.7.18
 // @author      OGSteam
 // @namespace	xtense.ogsteam.fr
 // @include     https://*.ogame.*/game/index.php*
@@ -10,7 +10,7 @@
 // @description Cette extension permet d'envoyer des données du jeu à votre serveur OGSPY d'alliance
 // ==/UserScript==
 // Variables Xtense
-var VERSION = '2.7.14';
+var VERSION = '2.7.18';
 var TYPE = 'GM-';
 var PLUGIN_REQUIRED = '2.7.3';
 var nomScript = 'Xtense';
@@ -34,7 +34,6 @@ if (isFirefox) {
 } else if (isOpera) {
     TYPE += 'OP';
 }
-GM_setValue('last_message', 0);
 
 /*************************** Init ***************************************/
 // Variables globales données ogame
