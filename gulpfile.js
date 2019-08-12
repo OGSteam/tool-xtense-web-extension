c24532923c3bd0ce2e4e36a40f9732d99797a3e2
darknoon29 <darknoon@darkcity.fr>
2019-08-12 17:54:53 +0200

    Build Package with Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,51 @@
-'use strict';
 
-var gulp = require('gulp');
-var replaceExt = require('replace-ext');
+const { series, parallel , src, dest } = require('gulp');
+const rename = require('gulp-rename');
+const zip = require('gulp-zip');
+const del = require('del');
+const manifest = require('read-pkg')
 
-gulp.task('lesspass', [], function () {
-    return gulp.src(['node_modules/lesspass-pure/dist/**/*', 'extension/popup.js'])
-        .pipe(gulp.dest('extension/dist/'));
-});
+// The `clean` function is not exported so it can be considered a private task.
+// It can still be used within the `series()` composition.
+function clean(cb) {
+    del(['release/**']);
+    console.log('Deleted files and directories: release/**');
+    cb();
+}
 
-gulp.task('build', [], function () {
-    //gulp.start('lesspass');
-    var path = '/some/dir/file.js';
-    var newPath = replaceExt(path, '.coffee');
-});
+function buid_for_firefox(cb){
+    //mv release/firefox/manifest.firefox release/firefox/manifest.json
+    //cross-var cross-zip release/firefox release/xtense-firefox-$npm_package_version.zip
+    src(['extension/**','!extension/manifest.*']).pipe(dest('release/firefox'));
+    src('extension/manifest.firefox').pipe(rename('manifest.json')).pipe(dest('release/firefox'));
+    console.log('Files copied to Firefox Release Folder');
 
-gulp.task('default', ['build'], function () {
 
-});
+    cb();
+}
+
+function buid_for_chrome(cb){
+    src(['extension/**','!extension/manifest.*']).pipe(dest('release/chrome'));
+    src('extension/manifest.chrome').pipe(rename('manifest.json')).pipe(dest('release/chrome'));
+    console.log('Files copied to Chrome Release Folder');
+    cb();
+}
+
+function package_for_chrome(cb){
+    src('release/chrome/*')
+        .pipe(zip('chrome-' + manifest.sync().version + '.zip'))
+        .pipe(dest('release'));
+    cb();
+}
+
+function package_for_firefox(cb){
+    src('release/firefox/*')
+        .pipe(zip('firefox-' + manifest.sync().version + '.zip'))
+        .pipe(dest('release'));
+    cb();
+}
+
+exports.clean = clean;
+exports.build = parallel(buid_for_firefox, buid_for_chrome );
+exports.package = parallel(package_for_chrome, package_for_firefox );
+exports.default = series(clean, buid_for_firefox, buid_for_chrome, package_for_chrome, package_for_firefox);
