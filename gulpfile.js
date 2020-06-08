e58ed2005890bfb70f106a468f0c026372ff7f4f
darknoon29 <darknoon@darkcity.fr>
2020-06-08 12:17:11 +0200

    Fix quotes

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,61 @@
 /* eslint no-console: "error" */
 
-const { series, parallel , src, dest } = require('gulp');
-const rename = require('gulp-rename');
-const zip = require('gulp-zip');
-const del = require('del');
-const manifest = require('read-pkg')
+const { series, parallel , src, dest } = require("gulp");
+const rename = require("gulp-rename");
+const zip = require("gulp-zip");
+const del = require("del");
+const manifest = require("read-pkg");
 
 // The `clean` function is not exported so it can be considered a private task.
 // It can still be used within the `series()` composition.
 function clean(cb) {
-    del.sync(['./release/**']);
-    console.log('Deleted files and directories: release/**');
+    del.sync(["./release/**"]);
+    console.log("Deleted files and directories: release/**");
     cb();
 }
 
 function update_jquery() {
-    return src(['node_modules/jquery/dist/jquery.min.js']).pipe(dest('extension/contribs'));
+    return src(["node_modules/jquery/dist/jquery.min.js"]).pipe(dest("extension/contribs"));
 }
 
 function update_loglevel() {
-    return src(['node_modules/loglevel/dist/loglevel.min.js']).pipe(dest('extension/contribs'));
+    return src(["node_modules/loglevel/dist/loglevel.min.js"]).pipe(dest("extension/contribs"));
 }
 
 function build(cb) {
     update_jquery();
     update_loglevel();
-    console.log('External Libs found and moved to extension folder');
+    console.log("External Libs found and moved to extension folder");
     cb();
 }
 
 function copy_files_for_firefox() {
-    return src(['extension/**','!extension/manifest.*']).pipe(dest('release/firefox'));
+    return src(["extension/**","!extension/manifest.*"]).pipe(dest("release/firefox"));
 }
 function copy_firefox_manifest() {
-    return src('extension/manifest.firefox').pipe(rename('manifest.json')).pipe(dest('release/firefox'));
+    return src("extension/manifest.firefox").pipe(rename("manifest.json")).pipe(dest("release/firefox"));
 }
 
 function copy_files_for_chrome() {
-    return src(['extension/**', '!extension/manifest.*']).pipe(dest('release/chrome'));
+    return src(["extension/**", "!extension/manifest.*"]).pipe(dest("release/chrome"));
 }
 function copy_chrome_manifest() {
-    return src('extension/manifest.chrome').pipe(rename('manifest.json')).pipe(dest('release/chrome'));
+    return src("extension/manifest.chrome").pipe(rename("manifest.json")).pipe(dest("release/chrome"));
 }
 
 function package_for_chrome(cb){
-    console.log('Ready to Zip Chrome Files');
-    src('release/chrome/**')
-        .pipe(zip('chrome-' + manifest.sync().version + '.zip'))
-        .pipe(dest('release'));
+    console.log("Ready to Zip Chrome Files");
+    src("release/chrome/**")
+        .pipe(zip("chrome-" + manifest.sync().version + ".zip"))
+        .pipe(dest("release"));
     cb();
 }
 
 function package_for_firefox(cb){
-    console.log('Ready to Zip Firefox Files');
-    src('release/firefox/**')
-        .pipe(zip('firefox-' + manifest.sync().version + '.zip'))
-        .pipe(dest('release'));
+    console.log("Ready to Zip Firefox Files");
+    src("release/firefox/**")
+        .pipe(zip("firefox-" + manifest.sync().version + ".zip"))
+        .pipe(dest("release"));
     cb();
 }
 
