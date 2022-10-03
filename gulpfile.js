a54ea9b6b86867cc2b678d7e910a60d7aa21c11c
darknoon29 <darknoon@darkcity.fr>
2022-10-03 13:55:25 +0200

    Update NPM Packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-const { series, parallel, src, dest } = require("gulp");
-const rename = require("gulp-rename");
-const zip = require("gulp-zip");
-const del = require("del");
-const manifest = require("read-pkg");
+import pkg from 'gulp';
+const { series, parallel, src, dest } = pkg;
+import rename from "gulp-rename";
+import zip from "gulp-zip";
+import { deleteAsync } from "del";
+import { readPackage } from "read-pkg";
 
 // The `clean` function is not exported so it can be considered a private task.
 // It can still be used within the `series()` composition.
 function clean(cb) {
-    del.sync(["./release/**"]);
+    deleteAsync(["./release/**"]);
     cb();
 }
 
@@ -62,31 +63,34 @@ function copy_edge_htmlfiles() {
 
 function package_for_chrome(cb) {
     src("release/chrome/**")
-        .pipe(zip("chrome-" + manifest.sync().version + ".zip"))
+        .pipe(zip("chrome-" + readPackage().version + ".zip"))
         .pipe(dest("release"));
     cb();
 }
 
 function package_for_firefox(cb) {
     src("release/firefox/**")
-        .pipe(zip("firefox-" + manifest.sync().version + ".zip"))
+        .pipe(zip("firefox-" + readPackage().version + ".zip"))
         .pipe(dest("release"));
     cb();
 }
 
 function package_for_edge(cb) {
     src("release/firefox/**")
-        .pipe(zip("edge-" + manifest.sync().version + ".zip"))
+        .pipe(zip("edge-" + readPackage().version + ".zip"))
         .pipe(dest("release"));
     cb();
 }
 
-exports.clean = clean;
-exports.build = build;
-exports.packchrome = series(parallel(copy_files_for_chrome, copy_chrome_manifest, copy_chrome_htmlfiles), package_for_chrome);
-exports.packfirefox = series(parallel(copy_files_for_firefox, copy_firefox_manifest, copy_firefox_htmlfiles), package_for_firefox);
-exports.packedge = series(parallel(copy_files_for_edge, copy_edge_manifest, copy_edge_htmlfiles), package_for_edge);
-exports.default = series(clean, build,
+const _clean = clean;
+export { _clean as clean };
+const _build = build;
+export { _build as build };
+export const packchrome = series(parallel(copy_files_for_chrome, copy_chrome_manifest, copy_chrome_htmlfiles), package_for_chrome);
+export const packfirefox = series(parallel(copy_files_for_firefox, copy_firefox_manifest, copy_firefox_htmlfiles), package_for_firefox);
+export const packedge = series(parallel(copy_files_for_edge, copy_edge_manifest, copy_edge_htmlfiles), package_for_edge);
+const _default = series(clean, build,
     parallel(series(parallel(copy_files_for_chrome, copy_chrome_manifest, copy_chrome_htmlfiles), package_for_chrome),
         series(parallel(copy_files_for_firefox, copy_firefox_manifest, copy_firefox_htmlfiles), package_for_firefox),
         series(parallel(copy_files_for_edge, copy_edge_manifest, copy_edge_htmlfiles), package_for_edge)));
+export { _default as default };
